84bc20cdb781b1582fcef46902a22c23048aa901
Joe <1790822+joenash@users.noreply.github.com>
2026-01-22 18:14:40 +0000

    cool-tool: add 0.1.0 bottle.

--- Formula/cool-tool.rb
+++ Formula/cool-tool.rb
@@ -5,6 +5,13 @@ class CoolTool < Formula
   sha256 "f923d6acfce087917ee92ea03ff8f0fd3ebdfeed0494379552c30c9351c88dff"
   license ""
 
+  bottle do
+    root_url "https://github.com/joenash/homebrew-tools/releases/download/cool-tool-0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "abad11ac0bc79e518dc38e1087bb6cb1f9c11181b16b1775a7b99351ee95281c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "75e628b0e1c79553507ab903a26d2093d74c2a80f061f1e7419782ca0014c604"
+  end
+
   depends_on "go" => :build
 
   def install
