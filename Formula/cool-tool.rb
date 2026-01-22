4a9a966c9f23f94578fdbe78d0a8b2db58a68125
Joe Nash <1790822+joenash@users.noreply.github.com>
2026-01-22 19:09:07 +0100

    Trying again

--- Formula/cool-tool.rb
+++ Formula/cool-tool.rb
@@ -1,5 +1,5 @@
 class CoolTool < Formula
-  desc "Just a thing for a demo, don't worry about it!"
+  desc "Just a thing for a demo, don't worry about it!!"
   homepage "https://github.com/joenash/cool-tool"
   url "https://github.com/joenash/cool-tool/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "f923d6acfce087917ee92ea03ff8f0fd3ebdfeed0494379552c30c9351c88dff"
